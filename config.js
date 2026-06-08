1eba833b0eb6be228d21d256ded7623a302cc036
Ximena Alexandra <ximenasantiagoo@outlook.com>
2026-06-08 14:03:17 -0600

    Add configuration for AWS re/Start documentation

---
+++ config.js
@@ -0,0 +1,7 @@
+window.$docsify = {
+  name: 'AWS re/Start',
+  repo: 'https://github.com/ximenaasantiagoo/laboratorios-aws', 
+  loadSidebar: true, 
+  coverpage: false,
+  homepage: 'README.md'
+}
