99e1bfe1d71bb2aabd0b06ab85d10b45ed846e56
Ximena Alexandra <ximenasantiagoo@outlook.com>
2026-06-08 14:33:12 -0600

    Update comments in config.js for clarity

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 window.$docsify = {
-  // El nombre que aparecerá en la esquina superior izquierda de la barra lateral
+  // Nombre que aparecerá en la esquina superior izquierda de la barra lateral
   name: 'AWS re/Start',
   
   // Agrega el icono de GitHub en la esquina superior derecha apuntando a tu repositorio
   repo: 'https://github.com/ximenaasantiagoo/laboratorios-aws', 
   
-  // TRUE le dice a Docsify: "Busca el archivo _sidebar.md y úsalo como menú lateral"
+  // Docsify: Busca el archivo _sidebar.md y crea el menú lateral 
   loadSidebar: true, 
   
-  // FALSE desactiva la página de portada (si fuera true, buscaría un archivo _coverpage.md)
+  // FALSE desactiva la página de portada
   coverpage: false,
   
   // Define cuál archivo Markdown debe usarse como la página de inicio por defecto
