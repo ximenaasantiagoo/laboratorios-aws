71daab51b0e101808fb544ff9dbe00c0b85f75c9
Ximena Alexandra <ximenasantiagoo@outlook.com>
2026-06-10 15:09:15 -0600

    Remove comments from config.js

--- config.js
+++ config.js
@@ -1,16 +1,6 @@
 window.$docsify = {
-  // Nombre que aparecerá en la esquina superior izquierda de la barra lateral
   name: 'AWS re/Start',
-  
-  // Agrega el icono de GitHub en la esquina superior derecha apuntando a tu repositorio
   repo: 'https://github.com/ximenaasantiagoo/laboratorios-aws', 
-  
-  // Docsify: Busca el archivo _sidebar.md y crea el menú lateral 
   loadSidebar: true, 
-  
-  // FALSE desactiva la página de portada
-  coverpage: false,
-  
-  // Define cuál archivo Markdown debe usarse como la página de inicio por defecto
   homepage: 'README.md'
 }
